009b754b4fb4e3478b420c09a87054bf6b9c539f
Nat Welch <nat@natwelch.com>
2012-08-07 10:16:28 -0700

    s/HelloAndroid/BrainDump

--- src/com/devcloud/braindump/BrainDump.java
+++ src/com/devcloud/braindump/BrainDump.java
@@ -3,7 +3,7 @@ package com.devcloud.braindump;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class HelloAndroid extends Activity
+public class BrainDump extends Activity
 {
     /** Called when the activity is first created. */
     @Override
