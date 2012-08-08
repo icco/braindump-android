38de0d977d7e258a906b8528d556b56d605342f0
Nat Welch <nat@natwelch.com>
2012-08-08 09:16:33 -0700

    dicking around.

--- src/com/devcloud/braindump/BrainDump.java
+++ src/com/devcloud/braindump/BrainDump.java
@@ -3,13 +3,12 @@ package com.devcloud.braindump;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class BrainDump extends Activity
-{
+public class BrainDump extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(android.R.layout.list_content);
     }
 }
