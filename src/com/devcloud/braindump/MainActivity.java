bb6273ac8a935458cdd87012643c03a05e00cc7e
Nat Welch <nat@natwelch.com>
2012-08-09 23:28:43 -0700

    Moving stuff around, creating stuff

--- src/com/devcloud/braindump/MainActivity.java
+++ src/com/devcloud/braindump/MainActivity.java
@@ -1,20 +1,21 @@
+
 package com.devcloud.braindump;
 
 import android.os.Bundle;
-import android.app.Activity;
+import org.apache.cordova.*;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 }
